12a43b84c03cf5504d6e928f2d9b7728df9d4197
Kion-Smith <kion_smith@hotmail.com>
2019-04-24 16:19:02 -0500

    Create functions.java

---
+++ Code/Episode 4/functions.java
@@ -0,0 +1,30 @@
+
+public class functions 
+{
+	
+	//public protected private 
+	//static 
+	//void returns nothing, int, whatever data type
+	//name
+	//variable with name
+	
+	public static void main(String[] args)
+	{
+		sayHello();
+	}
+	
+	
+	public static void sayHello()
+	{
+		System.out.print("Hello");
+	}
+	
+}
+
+class help
+{
+	public help()
+	{
+		
+	}
+}
